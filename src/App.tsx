af998597570023920c677535e610140895d18d21
Evan <dev@perfectcode.ai>
2023-08-14 23:34:33 +0200

    added test setup, prettier, eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
@@ -18,7 +18,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
---
+++ src/example.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from 'vitest'
+
+describe('test setup with an example', () => {
+  test('Test value is true', () => {
+    const val = true
+    expect(val).toBeTruthy()
+  })
+})
